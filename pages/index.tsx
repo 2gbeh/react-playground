af5b8a090d51cbee0e76c7c8f060e8832d6ced79
etugbeh-plateaumed <etugbeh@plateaumed.com>
2024-11-30 08:29:05 +0100

    feat(components): add templates dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
       <Ribbon title="Dashboard" />
       <main>
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          {/* Your content */}
         </div>
       </main>
     </div>
